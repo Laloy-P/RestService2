be4d70c564215a3484e7d6ef95697cbec4b3ae3d
Laloy-P <pi.laloy@laposte.net>
2018-05-10 15:43:40 +0200

    First commit to implement addService

--- src/org/cloudProject/services/AddApprovalService.java
+++ src/org/cloudProject/services/AddApprovalService.java
@@ -1,14 +1,19 @@
 package org.cloudProject.services;
 
+import java.io.IOException;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 
 @Path("/AddApproval")
 public class AddApprovalService {
-
+	
 	@GET
 	@Produces("application/xml")
 	public String getListApproval() {
@@ -17,4 +22,17 @@ public class AddApprovalService {
 
 	}
 
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newTodo(@FormParam("id") String id, @FormParam("response") boolean response,
+			@FormParam("description") String description)
+			throws IOException {
+			System.out.println(id);
+			System.out.println(description);
+			System.out.println(response);
+				
+
+				
+	}
 }
