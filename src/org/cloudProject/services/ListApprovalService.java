718976b169fa00560b37d57c133dfbdf633002d7
Laloy-P <pi.laloy@laposte.net>
2018-05-14 18:01:01 +0200

    Going from xml to json

--- src/org/cloudProject/services/ListApprovalService.java
+++ src/org/cloudProject/services/ListApprovalService.java
@@ -1,6 +1,7 @@
 package org.cloudProject.services;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -14,54 +15,18 @@ import org.cloudProject.beans.Approval;
 
 @Path("/ListApprovals")
 public class ListApprovalService {
-/*	
-	@GET
-	@Produces("application/xml")
-	public String getListApproval() {
-		
-		StringBuilder approvals = new StringBuilder();
-		approvals.append("<ListApprovalService>");
-		approvals.append("<Approvals>");
-		if (ApprovalDB.listApproval() != null) {
-			for (Approval element : ApprovalDB.listApproval()) {
-				
-				approvals.append("<Approval>");
-					
-					approvals.append("<id>");
-						approvals.append(element.getId());
-					approvals.append("</id>");
-					
-					approvals.append("<name>");
-						approvals.append(element.getName());
-					approvals.append("</name>");
-					
-					approvals.append("<description>");
-						approvals.append(element.getDescription());
-					approvals.append("</description>");
-					
-					approvals.append("<response>");
-						approvals.append(element.getResponse());
-					approvals.append("</response>");
-					
-				approvals.append("</Approval>");
-			}
-		}else {
-			approvals.append("<Approval>");
-			approvals.append("No approval has been found");
-			approvals.append("</Approval>");
-		}
-		approvals.append("</Approvals>");
-		approvals.append("</ListApprovalService>");
-		
-		Response.ok();
-		return  approvals.toString() ;
 
-	}
-*/	
 	 @GET
-	 @Produces(MediaType.APPLICATION_XML)
-	 public List<Approval> responseMsg( ) {
-		 	
+	 @Produces (MediaType.APPLICATION_JSON)
+	 public List<Approval> getXMLApprovals() {
+		 /*
+		 List<Approval>localTest = new ArrayList<>();
+		 localTest.add(new Approval(1,"bleu","bleu","bleu"));
+		 localTest.add(new Approval(2,"rouge","rouge","rouge"));
+		 localTest.add(new Approval(3,"vert","vert","vert"));
+		 
+		 return localTest;
+		 */
 		 if(ApprovalDB.listApproval() != null) {
 		 		return ApprovalDB.listApproval();
 		 }
@@ -69,7 +34,8 @@ public class ListApprovalService {
 		 	Response.ok();
 			
 		 	return null;
-	 
-	     }
+		 	
+	 }
+
 	
 }
